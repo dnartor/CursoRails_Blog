9a53ec1bd333b6a8ffc1c49b0b6eb80934fd7ab7
DanielNaranjo <dnartor.98@gmail.com>
2021-04-07 14:28:49 +0100

    added categories

--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,20 @@ Rails
   .draw do
     devise_for :users
     get 'home/index'
-
-    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     
-    get "articles/user/:user_id", to: "articles#from_author"
-    get '/bienvenida', to: 'home#index'
     root to: "home#index"
-    get "articles", to:"articles#index"
-    get '/articles/new', to: 'articles#new', as: :new_articles
-    get '/articles/:id', to: 'articles#show'
-    get '/articles/:id/edit', to: 'articles#edit'
-    patch "/articles/:id", to: "articles#update", as: :article
-    post 'articles', to: 'articles#create'
-    delete '/articles/:id', to:'articles#destroy'
+    
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    resources :categories
+    resources :articles do
+        get "user/:user_id", to: "articles#from_author", on: :collection
+    end
+    # get '/bienvenida', to: 'home#index'
+    # get "articles", to:"articles#index"
+    # get '/articles/new', to: 'articles#new', as: :new_articles
+    # get '/articles/:id', to: 'articles#show'
+    # get '/articles/:id/edit', to: 'articles#edit'
+    # patch "/articles/:id", to: "articles#update", as: :article
+    # post 'articles', to: 'articles#create'
+    # delete '/articles/:id', to:'articles#destroy'
   end
